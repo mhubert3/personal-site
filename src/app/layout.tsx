edac519e73b74897c0e100fc31f33a9974693ef7
mhubert3 <146325770+mhubert3@users.noreply.github.com>
2025-05-13 03:41:46 -0400

    updated layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/Navbar";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Morgan Huberty",
@@ -23,7 +12,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode}>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <head>
       </head>
       <body
